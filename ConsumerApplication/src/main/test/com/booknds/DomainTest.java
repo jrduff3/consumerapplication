a2fb9c7fa98efe0dbff260247d835365dba8de52
jduff3 <john.duff@cprime.com>
2017-05-12 15:10:38 -0400

    added junit tests for sonarqube

---
+++ ConsumerApplication/src/main/test/com/booknds/DomainTest.java
@@ -0,0 +1,41 @@
+package com.booknds;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import com.booknds.domain.Consumer;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class DomainTest
+        extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DomainTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( DomainTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+{
+    assertTrue( false );
+}
+
+}
