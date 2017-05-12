84dc3ee4c8984d6455d8e7e138675b2a6713ffa0
jduff3 <john.duff@cprime.com>
2017-05-12 15:30:11 -0400

    relocated junit tests

---
+++ ConsumerApplication/src/test/java/com/booknds/ControllerTest.java
@@ -0,0 +1,41 @@
+package com.booknds;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import com.booknds.domain.Consumer;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class ControllerTest
+        extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ControllerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ControllerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( false );
+    }
+
+}
